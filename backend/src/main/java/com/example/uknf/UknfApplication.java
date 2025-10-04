affb04b4fe974773e8c1e3da9c2cfb91d0d76e9c
benhus8 <benedykt.huszcza@gmail.com>
2025-10-04 16:06:15 +0200

    refactor project structure and add initial configuration files

---
+++ backend/src/main/java/com/example/uknf/UknfApplication.java
@@ -0,0 +1,13 @@
+package com.example.uknf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UknfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UknfApplication.class, args);
+	}
+
+}
