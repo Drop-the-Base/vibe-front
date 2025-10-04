114e216742040331ced03583132a1efba8985ab1
benhus8 <benedykt.huszcza@gmail.com>
2025-10-04 21:32:45 +0200

    Add Spring Data REST support for SupervisedEntity repository

--- backend/src/main/java/com/example/uknf/repositories/SupervisedEntityRepository.java
+++ backend/src/main/java/com/example/uknf/repositories/SupervisedEntityRepository.java
@@ -2,6 +2,7 @@ package com.example.uknf.repositories;
 
 import com.example.uknf.entities.SupervisedEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface SupervisedEntityRepository extends JpaRepository<SupervisedEntity, Integer> {
-}
+@RepositoryRestResource(path = "entities", collectionResourceRel = "entities")
+public interface SupervisedEntityRepository extends JpaRepository<SupervisedEntity, Integer> { }
