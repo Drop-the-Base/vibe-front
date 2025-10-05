a19c980d80713efb119abcb701cac11adedec537
benhus8 <benedykt.huszcza@gmail.com>
2025-10-05 10:59:56 +0200

    reduce wait time for SQL Server readiness check in init-mssql.sh

--- init-mssql.sh
+++ init-mssql.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "⏳ Czekam na gotowość SQL Servera..."
 # Pętlą czekamy aż zacznie odpowiadać (max ~2 min)
-for i in {1..60}; do
+for i in {1..10}; do
   if /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -C -Q "SELECT 1" >/dev/null 2>&1; then
     break
   fi
